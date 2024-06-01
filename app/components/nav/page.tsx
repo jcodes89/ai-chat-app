c3a7760c59f51c7ab36dfa2c07a418222a306b26
jcodes89 <jeremy.sluder89@gmail.com>
2024-06-01 10:26:48 -0400

    creation of form and input field

---
+++ app/components/nav/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import img from '@/public/Images/Firefly A detailed illustration of an all-knowing and powerful Dungeons and Dragons wizard to be use.jpg'
+
+
+const Nav = () => {
+  return (
+    <header>
+        <nav className="h-fit p-2 bg-black text-gray-50"> 
+        <div className="flex flex-row items-center justify-between">
+          <Image src={img} alt="image of all knowing wizard from dungeons and dragons" className="h-28 w-auto rounded-full "/>
+          <h1 className="h-fit mr-4 font-extrabold text-xl">Chatbot</h1>
+        </div>
+        </nav>
+      </header>
+  )
+}
+
+export default Nav
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+'use client'
+import { log } from "console";
+import Nav from "./components/nav/page";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <main>
-      <h2 className="btn">Main page</h2>
-    </main>
+    <>
+      <Nav/>
+      <main className="flex flex-col h-full w-auto justify-center">
+        <div className="m-3 border-black border-2 bg-gray-200">
+          <p className="p-4"></p>
+        </div>
+        <form onSubmit={handleSubmit} className="h-40 w-full flex md:flex-row flex-col justify-center items-center px-4">
+          <textarea onChange={(e) => {
+            setMessage(e.target.value)
+            
+          }} typeof="text" value={message} placeholder="Please type your message here ..." className="h-32 bg-gray-100 rounded-md outline-none p-4 w-full resize-none placeholder:text-black "/>
+          <button type="submit" className="btn md:ml-3 mt-3">Submit</button>
+        </form>
+      </main>
+    </>
   );
 }
