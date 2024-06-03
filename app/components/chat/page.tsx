7131d37b3732a066a48542f9c096433d4eaa516e
jcodes89 <jeremy.sluder89@gmail.com>
2024-06-03 09:36:03 -0400

    update to chat page

--- app/components/chat/page.tsx
+++ app/components/chat/page.tsx
@@ -15,7 +15,7 @@ const Chat = () => {
             <div className="mt-4 ">
                 {messages.map(message => (
                     <div key={message.id} className={message.content  ? 'bg-gray-300 p-4 m-4 rounded-md' : 'bg-none hidden'}>
-                        {message.role === 'user' ? 'User: ' : 'AI: '}
+                        {message.role === 'user' ? 'User: ' : 'Grand Master: '}
                         <Image alt="image of avatar" src={message.role === 'user' ? userAvatar : chatAvatar} width={50} height={50}/>
                         {message.content}
                     </div>
@@ -28,7 +28,7 @@ const Chat = () => {
     <>
         {renderedResponse()}
         <form onSubmit={handleSubmit} className="h-auto w-full flex md:flex-row flex-col justify-center items-center px-4">
-          <textarea onChange={handleInputChange} typeof="text" value={input} placeholder="Lets embark on your journey together ..." className="h-32 bg-gray-100 text-gray-100 rounded-md outline-none p-4 w-full resize-none placeholder:text-black "/>
+          <textarea onChange={handleInputChange} typeof="text" value={input} placeholder={messages ? 'Message Grand Master' : "Lets embark on your journey together ..." className="h-32 bg-gray-100 text-gray-100 rounded-md outline-none p-4 w-full resize-none placeholder:text-black "/>
           <button type="submit" className="btn md:ml-3 my-3">Submit</button>
         </form>
     </>
