c91a8bf29078a922dde8c59e3e52252b8cf1ab96
jcodes89 <jeremy.sluder89@gmail.com>
2024-06-09 12:48:11 -0400

    update to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/nav/page";
-
+import Link from "next/link";
+import img from '@/public/Images/grandmaster-image.webp'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI chat app",
-  description: "App uses AI to create prompts",
+  title: "AI Quest Master - Chatbot for Dungeons and Dragons ",
+  description: `AI Quest Master is an innovative chatbot designed to enhance the Dungeons and Dragons experience.  The AI acts as the Dungeon Master. This cutting-edge application leverages advanced natural language processing to create immersive narratives tailored to each player's actions and decisions. With seamless integration of modern web technologies and the OpenAI API, AI Dungeon Master offers dynamic storytelling, character creation options, and an engaging gameplay experience. Embark on epic adventures, shape your own destiny, and experience the thrill of Dungeons and Dragons like never before.
+Keywords: AI, Chatbot, Dungeons and Dragons, RPG, Role-playing Game, OpenAI, Tailwind, JavaScript, React.js, Next.js, Storytelling, Character Creation, Immersive Experience. D & D QuestMaster is not affiliated with Wizards of the Cost or Dungeons and Dragons. `,
+  
 };
 
 export default function RootLayout({
@@ -19,7 +22,7 @@ export default function RootLayout({
     <html lang="en" className="">
       <head>
       <script src="https://kit.fontawesome.com/5c59429581.js" defer crossOrigin="anonymous"></script>
-
+      <link rel="icon" type="image/x-icon" href="/Images/grandmaster-image.webp" className="rounded-full scale-150"></link>
       </head>
       <body className={`${inter.className} `}>
         <Nav/>
